04a6105de9ee6452d652db7792dcf188d67ff37f
eRepetto <37150582+eRepetto@users.noreply.github.com>
2019-01-24 12:45:56 -0500

    added Blink_led.ino

---
+++ Blink_led.ino
@@ -0,0 +1,55 @@
+
+const byte ledPin = 20; // 13 / 20
+const byte buttonPin = 9;
+byte buttonState = 0;
+boolean lightOn  = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ //works holding the button   
+ buttonState = digitalRead (buttonPin);
+if(buttonState == HIGH){
+  digitalWrite(ledPin, HIGH);
+} else{
+  digitalWrite(ledPin, LOW);    
+  }
+ 
+   //led pin number 13   
+  /*
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(1000);                  // wait for a second  
+  */
+
+
+
+
+
+
+  
+  //switch
+  /*
+buttonState = digitalRead (buttonPin);
+if(buttonState == HIGH){
+ if(lightOn== false)
+ lightOn = true;
+ else
+ lightOn = false;  
+}
+
+
+if(lightOn)
+  digitalWrite(ledPin, HIGH);
+  else
+  digitalWrite(ledPin, LOW);
+
+delay(100);
+  
+*/
+}
